7a30ba47c0edb67b1072e9bc1ee3c66069685a10
Aj7ay <ajayyegireddi007@gmail.com>
2023-06-15 10:59:31 +0530

    Python calculator

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    num1 = float(request.form['num1'])
+    num2 = float(request.form['num2'])
+    operator = request.form['operator']
+
+    if operator == '+':
+        result = num1 + num2
+    elif operator == '-':
+        result = num1 - num2
+    elif operator == '*':
+        result = num1 * num2
+    elif operator == '/':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = 'Error: Division by zero!'
+    else:
+        result = 'Error: Invalid operator!'
+
+    return render_template('result.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
